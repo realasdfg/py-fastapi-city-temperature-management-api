8af685217034c68d9a55ba5b1c37e2a7f7ce0c0d
Roman Popadiuk <realasdfga@gmail.com>
2026-07-25 01:05:20 +0200

    Implement /temperatures endpoint

--- temperatures/crud.py
+++ temperatures/crud.py
@@ -1,3 +1,6 @@
+from typing import Sequence
+
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from temperatures.models import Temperature
@@ -11,3 +14,13 @@ async def create_temperatures(
     session.add_all(temperatures)
     await session.flush()
     return temperatures
+
+
+async def get_all_temperatures(
+    session: AsyncSession, city_id: int | None = None
+) -> Sequence[Temperature]:
+    stmt = select(Temperature)
+    if city_id is not None:
+        stmt = stmt.where(Temperature.city_id == city_id)
+    result = await session.execute(stmt)
+    return result.scalars().all()
--- temperatures/router.py
+++ temperatures/router.py
@@ -1,14 +1,16 @@
 import asyncio
 from datetime import datetime
+from typing import List
 
 import httpx
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from cities.crud import get_all_cities
 from database import get_async_session
-from temperatures.crud import create_temperatures
+from temperatures.crud import create_temperatures, get_all_temperatures
 from temperatures.schemas import (
+    STemperature,
     STemperatureCreate,
     STemperatureUpdateResult,
 )
@@ -71,3 +73,12 @@ async def update_temperatures(
         await session.commit()
 
     return response
+
+
+@router.get("/")
+async def get_temperatures(
+    city_id: int = Query(int, alias="city_id"),
+    session: AsyncSession = Depends(get_async_session),
+) -> List[STemperature]:
+    temperatures = await get_all_temperatures(session=session, city_id=city_id)
+    return [STemperature.model_validate(temp) for temp in temperatures]
