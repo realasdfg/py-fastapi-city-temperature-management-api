0b6396ec8842df1215852c507abcb01c571efa67
Roman Popadiuk <realasdfga@gmail.com>
2026-07-24 18:16:52 +0200

    Create database.py

---
+++ database.py
@@ -0,0 +1,17 @@
+from typing import Any, AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import declarative_base
+
+SQLALCHEMY_DATABASE_URI = "sqlite+aiosqlite:///db.sqlite3"
+
+engine = create_async_engine(SQLALCHEMY_DATABASE_URI)
+
+Base = declarative_base()
+
+async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, Any]:
+    async with async_session() as session:
+        yield session
